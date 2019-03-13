428793514d9cfb436149f0f84a95ea3871888925
Felipe Sens Bonetto <=>
2019-03-13 09:03:10 -0300

    init

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get  'static_pages/about'
+  get  'static_pages/contacts'
+  root 'application#hello'
+end
